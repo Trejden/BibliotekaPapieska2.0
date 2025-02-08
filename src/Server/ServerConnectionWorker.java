773f9f36a5fc54f3c88a1a86db3291d0786a55aa
Trejden <michamel08@gmail.com>
2025-02-08 21:08:54 +0100

    stuff

--- src/Server/ServerConnectionWorker.java
+++ src/Server/ServerConnectionWorker.java
@@ -1,12 +1,19 @@
 package Server;
 
+import org.json.JSONObject;
+
 import java.io.*;
 import java.net.Socket;
 
 public class ServerConnectionWorker implements Runnable{
     protected Socket clientSocket = null;
+    private boolean isAuthorized = false;
     public ServerConnectionWorker(Socket clientSocket) {
         this.clientSocket = clientSocket;
+    }
+
+    private void HandleLoginCommand(JSONObject command) {
+
     }
 
     public void run() {
@@ -14,6 +21,26 @@ public class ServerConnectionWorker implements Runnable{
             System.out.println("Rozpoczęto obsługę połączenia");
             PrintWriter  out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader  in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                JSONObject command = new JSONObject(inputLine);
+                int commandType = command.getInt("Command");
+                switch (commandType){
+                    case 0:
+                        System.out.println("Otrzymano komende logowania");
+                        HandleLoginCommand(command);
+                        break;
+                    case 1:
+                        System.out.println("Otrzymano komende utworzenia użytkownika");
+                        break;
+                    case 2:
+                        System.out.println("Otrzymano komende dodania książki");
+                        break;
+                    default:
+                        System.out.println("Nieznany typ komendy: " + commandType);
+                        break;
+                }
+            }
             out.close();
             in.close();
         } catch (IOException e) {
